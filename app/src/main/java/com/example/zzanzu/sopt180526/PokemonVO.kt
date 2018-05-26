915f65d0de224dd658de598842309c899663ae71
ZZANZU <lees3301@gmail.com>
2018-05-26 17:13:09 +0900

    Edited MainActivity

--- app/src/main/java/com/example/zzanzu/sopt180526/PokemonVO.kt
+++ app/src/main/java/com/example/zzanzu/sopt180526/PokemonVO.kt
@@ -3,9 +3,9 @@ package com.example.zzanzu.sopt180526
 import io.realm.RealmObject
 import io.realm.annotations.PrimaryKey
 
-class PokemonVO : RealmObject() {
+open class PokemonVO : RealmObject() {
     @PrimaryKey
-    var name : String = ""
+    var name : String = "" // main key!
     var num : Int = 0
     var user : String = ""
     var type : String = ""
